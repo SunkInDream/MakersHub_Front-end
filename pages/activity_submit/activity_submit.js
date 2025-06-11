79c9e87b4350d896851e124afd1b2f21128d7f0c
Lumiat <grace20177102@icloud,com>
2025-06-11 16:54:16 +0800

    :construction: submit new activity function constructed

--- pages/activity_submit/activity_submit.js
+++ pages/activity_submit/activity_submit.js
@@ -1,294 +1,387 @@
 // pages/activity/activity.js
 const token = wx.getStorageSync('auth_token');
+const API_BASE = "http://146.56.227.73:8000";
+// 导入外部util工具
+const utils = require('../../utils/util')
 
 Page({
   data: {
-    // 初始化格式化的时间对象
+    // 表单数据
+    event: {
+      event_name: '',
+      poster: '',       // 本地图片路径
+      posterUrl: '',    // 后端返回的URL (只在提交时才会获取)
+      description: '',
+      location: '',
+      link: '',
+      participant: '',
+      start_time: '', 
+      end_time: '',
+      registration_deadline: ''
+    },
+    
+    // 其他数据保持不变
+    timePicker: {
+      startTime: { year: '', month: '', day: '', hour: '', minute: '' },
+      endTime: { year: '', month: '', day: '', hour: '', minute: '' },
+      deadline: { year: '', month: '', day: '', hour: '', minute: '' }
+    },
     formattedStartTime: { year: '年', month: '月', day: '日', hour: '时', minute: '分' },
     formattedEndTime: { year: '年', month: '月', day: '日', hour: '时', minute: '分' },
     formattedDeadline: { year: '年', month: '月', day: '日', hour: '时', minute: '分' },
-    event_name: '',
-    poster: '',
-    description: '',
-    location: '',
-    link: '',
-    years: Array.from({ length: 100 }, (_, i) => 2024 + i),
-    months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-    days: Array.from({ length: 31 }, (_, i) => i + 1),
-    hours: Array.from({ length: 24 }, (_, i) => i + 1),
-    minutes: Array.from({ length: 59 }, (_, i) => i + 1),
-    startTimeYear: "年",
-    startTimeMonth: "月",
-    startTimeDay: "日",
-    startTimeHour: "时",
-    startTimeMinute: "分",
-    endTimeYear: "年",
-    endTimeMonth: "月",
-    endTimeDay: "日",
-    endTimeHour: "时",
-    endTimeMinute: "分",
-    deadlineYear: "年",
-    deadlineMonth: "月",
-    deadlineDay: "日",
-    deadlineHour: "时",
-    deadlineMinute: "分",
-    start_time:"",
-    end_time:"",
-    registration_deadline:"",
-
-    screenWidth: 0,
-    screenHeight: 0,
-    layoutStyle: '',
+    options: {
+      years: Array.from({ length: 100 }, (_, i) => 2024 + i),
+      months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      days: Array.from({ length: 31 }, (_, i) => i + 1 + '日'),
+      hours: Array.from({ length: 24 }, (_, i) => i + '时'),
+      minutes: Array.from({ length: 60 }, (_, i) => i + '分')
+    },
     isNameFocused: false,
     isDescriptionFocused: false,
     isLocationFocused: false,
-    isLinkFocused: false
+    isLinkFocused: false,
+    isParticipantFocused: false,
+    isValid: true
   },
 
-  onLoad() {
-    // 页面加载时的逻辑
-   
-  },
-
-  handlerGobackClick() {
-    wx.showModal({
-      title: '你点击了返回',
-      content: '是否确认放回',
-      success: e => {
-        if (e.confirm) {
-          const pages = getCurrentPages();
-          if (pages.length >= 2) {
-            wx.navigateBack({
-              delta: 1
-            });
-          } else {
-            wx.reLaunch({
-              url: '/pages/index/index'
-            });
-          }
-        }
-      }
+  // 输入框处理函数保持不变...
+  onNameFocused() { this.setData({ isNameFocused: true }); },
+  onNameBlur(e) { 
+    this.setData({ 
+      isNameFocused: false,
+      'event.event_name': e.detail.value
     });
   },
-  handlerGohomeClick() {
-    wx.reLaunch({
-      url: '/pages/index/index'
+  
+  onDescriptionFocused() { this.setData({ isDescriptionFocused: true }); },
+  onDescriptionBlur(e) { 
+    this.setData({ 
+      isDescriptionFocused: false,
+      'event.description': e.detail.value 
     });
   },
-
-  onNameFocused : function() {
+  
+  onLocationFocused() { this.setData({ isLocationFocused: true }); },
+  onLocationBlur(e) { 
+    this.setData({ 
+      isLocationFocused: false,
+      'event.location': e.detail.value 
+    });
+  },
+  
+  onLinkFocused() { this.setData({ isLinkFocused: true }); },
+  onLinkBlur(e) { 
+    this.setData({ 
+      isLinkFocused: false,
+      'event.link': e.detail.value 
+    });
+  },
+  
+  onParticipantFocused() { this.setData({ isParticipantFocused: true }); },
+  onParticipantBlur(e) { 
     this.setData({
-      isNameFocused: true
-    });
-  },
-
-  onNameBlur : function() {
-    this.setData({
-      isNameFocused: false
-    });
-  },
-
-  onDescriptionFocused : function() {
-    this.setData({
-      isDescriptionFocused: true
-    });
-  },
-
-  onDescriptionBlur : function() {
-    this.setData({
-      isDescriptionFocused: false
-    });
-  },
-
-  onLocationFocused : function() {
-    this.setData({
-      isLocationFocused: true
-    });
-  },
-
-  onLocationBlur : function() {
-    this.setData({
-      isLocationFocused: false
-    });
-  },
-
-  onLinkFocused : function() {
-    this.setData({
-      isLinkFocused: true
-    });
-  },
-
-  onLinkBlur : function() {
-    this.setData({
-      isLinkFocused: false
+      isParticipantFocused: false,
+      'event.participant': e.detail.value
     });
   },
 
   onInput(e) {
     const field = e.currentTarget.dataset.field;
     this.setData({
-      [field]: e.detail.value,
-    });
-  },//输入函数
-
-  onUploadPoster() {//图片上传函数
-    const that = this;
-    wx.chooseImage({
-      count: 1,
-      success(res) {
-        const tempFilePaths = res.tempFilePaths;
-        that.setData({
-          poster: tempFilePaths[0],
-        });
-      },
+      [`event.${field}`]: e.detail.value,
     });
   },
 
-  // 时间格式化方法
+  // 修改：选择海报但不上传
+  onUploadPoster() {
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const tempFilePath = res.tempFiles[0].tempFilePath;
+        this.setData({ 
+          'event.poster': tempFilePath,
+          'event.posterUrl': '' // 清空posterUrl，因为还未上传
+        });
+      },
+      fail: (err) => {
+        console.error('选择图片失败:', err);
+      }
+    });
+  },
+
+  // 添加：上传海报的方法，返回Promise
+  uploadPoster(filePath) {
+    return new Promise((resolve, reject) => {
+      if (!filePath) {
+        reject('没有选择海报');
+        return;
+      }
+
+      wx.uploadFile({
+        url: `${API_BASE}/events/poster`,
+        filePath: filePath,
+        name: 'file',
+        header: {
+          'Authorization': `Bearer ${token}`
+        },
+        success: (res) => {
+          try {
+            const result = JSON.parse(res.data);
+            if (result.code === 200 && result.data && result.data.poster) {
+              resolve(result.data.poster);
+            } else {
+              reject(result.message || '上传失败');
+            }
+          } catch (e) {
+            reject('解析响应失败');
+          }
+        },
+        fail: (err) => {
+          reject(err);
+        }
+      });
+    });
+  },
+
+  // 时间相关函数保持不变
   updateFormattedTime(type) {
+    const timeData = this.data.timePicker[type];
+    
     const data = {
-      [`formatted${type.replace(/^\w/, c => c.toUpperCase())}`]: {
-        year: this.data[`${type}Year`] ? this.data[`${type}Year`].toString().replace('年', '') + '年' : '年',
-        month: this.data[`${type}Month`] ? this.data[`${type}Month`].replace('月','') + '月' : '月',
-        day: this.data[`${type}Day`] ? this.data[`${type}Day`].toString().replace('日', '') + '日' : '日',
-        hour: this.data[`${type}Hour`] ? this.data[`${type}Hour`].toString().replace('时', '') + '时' : '时',
-        minute: this.data[`${type}Minute`] ? this.data[`${type}Minute`].toString().replace('分', '') + '分' : '分'
+      [`formatted${type.charAt(0).toUpperCase() + type.slice(1)}`]: {
+        year: timeData.year ? timeData.year.toString() + '年' : '年',
+        month: timeData.month || '月',
+        day: timeData.day || '日',
+        hour: timeData.hour || '时',
+        minute: timeData.minute || '分'
       }
     };
     this.setData(data);
+    
+    this.updateISOTime(type);
   },
 
-    // 时间处理统一方法
-    formatTime(type) {
-      const formatUnit = (value) => value.toString().replace(/[^0-9]/g, '').padStart(2, '0');
+  updateISOTime(type) {
+    const timeData = this.data.timePicker[type];
+    // 假设formatToISOTime已定义
+    const isoTime = utils.formatToISOTime(timeData);
+
+    if (isoTime) {
+      const field = type === 'startTime' ? 'start_time' : 
+                  type === 'endTime' ? 'end_time' : 'registration_deadline';
+                  
+      this.setData({
+        [`event.${field}`]: isoTime
+      });
+    }
+  },
+
+  // 时间选择器事件保持不变
+  onYearChange(e) {
+    const type = e.currentTarget.dataset.type;
+    const value = this.data.options.years[e.detail.value];
+    this.setData({ 
+      [`timePicker.${type}.year`]: value 
+    }, () => this.updateFormattedTime(type));
+  },
+
+  onMonthChange(e) {
+    const type = e.currentTarget.dataset.type;
+    const value = this.data.options.months[e.detail.value];
+    this.setData({ 
+      [`timePicker.${type}.month`]: value 
+    }, () => this.updateFormattedTime(type));
+  },
+
+  onDayChange(e) {
+    const type = e.currentTarget.dataset.type;
+    const value = this.data.options.days[e.detail.value];
+    this.setData({ 
+      [`timePicker.${type}.day`]: value 
+    }, () => this.updateFormattedTime(type));
+  },
+
+  onHourChange(e) {
+    const type = e.currentTarget.dataset.type;
+    const value = this.data.options.hours[e.detail.value];
+    this.setData({ 
+      [`timePicker.${type}.hour`]: value 
+    }, () => this.updateFormattedTime(type));
+  },
+
+  onMinuteChange(e) {
+    const type = e.currentTarget.dataset.type;
+    const value = this.data.options.minutes[e.detail.value];
+    this.setData({ 
+      [`timePicker.${type}.minute`]: value 
+    }, () => this.updateFormattedTime(type));
+  },
+
+  validateForm() {
+    console.log("开始表单验证...");
+    try {
+      const event = this.data.event;
       
-      const timeData = {
-        year: this.data[`${type}Year`],
-        month: this.data[`${type}Month`],
-        day: this.data[`${type}Day`],
-        hour: this.data[`${type}Hour`],
-        minute: this.data[`${type}Minute`]
-      };
-  
-      return {
-        iso: `${timeData.year}-${formatUnit(timeData.month)}-${formatUnit(timeData.day)} ` +
-             `${formatUnit(timeData.hour)}:${formatUnit(timeData.minute)}:00`,
-        display: `${timeData.year}年${timeData.month}${timeData.day}日 ` +
-                 `${timeData.hour}时${timeData.minute}分`
-      };
-    },
-
-      // 时间选择事件示例
-    onYearChange(e) {
-      const type = e.currentTarget.dataset.type;
-      const value = this.data.years[e.detail.value];
-      this.setData({
-        [`${type}Year`]: value
-      }, () => {
-        this.updateFormattedTime(type);
-      });
-    },
-
-        // 月份选择
-    onMonthChange(e) {
-      const type = e.currentTarget.dataset.type;
-      const value = this.data.months[e.detail.value];
-      this.setData({
-        [`${type}Month`]: value
-      }, () => {
-        this.updateFormattedTime(type);
-      });
-    },
-
-    // 日期选择
-    onDayChange(e) {
-      const type = e.currentTarget.dataset.type;
-      const value = this.data.days[e.detail.value];
-      this.setData({
-        [`${type}Day`]: value
-      }, () => {
-        this.updateFormattedTime(type);
-      });
-    },
-
-    // 小时选择
-    onHourChange(e) {
-      const type = e.currentTarget.dataset.type;
-      const value = this.data.hours[e.detail.value];
-      this.setData({
-        [`${type}Hour`]: value
-      }, () => {
-        this.updateFormattedTime(type);
-      });
-    },
-
-    // 分钟选择
-    onMinuteChange(e) {
-      const type = e.currentTarget.dataset.type;
-      const value = this.data.minutes[e.detail.value];
-      this.setData({
-        [`${type}Minute`]: value
-      }, () => {
-        this.updateFormattedTime(type);
-      });
-    },
-
-    onSubmit() {
-      const app = this;
-      
-      // 表单验证逻辑
-      if (!this.data.event_name || 
-          !this.data.poster || 
-          !this.data.description || 
-          !this.data.location || 
-          !this.data.link) {
-        return wx.showToast({ title: '请填写完整信息', icon: 'none' });
+      // 手动检查每个必填字段
+      if (!event.event_name) {
+        console.log("活动名称验证失败");
+        wx.showToast({ title: '请填写活动名称', icon: 'none' });
+        this.data.isValid = false;
       }
-    
+      
+      if (!event.poster) {
+        console.log("海报验证失败");
+        wx.showToast({ title: '请选择活动海报', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.description) {
+        console.log("简介验证失败");
+        wx.showToast({ title: '请填写活动简介', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.location) {
+        console.log("地点验证失败");
+        wx.showToast({ title: '请填写举办地点', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.link) {
+        console.log("链接验证失败");
+        wx.showToast({ title: '请填写二课链接', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.participant) {
+        console.log("参与对象验证失败");
+        wx.showToast({ title: '请填写参与对象', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
       // 时间验证
-      ['startTime', 'endTime', 'deadline'].forEach(type => {
-        if (!this.data[`${type}Year`] || 
-            !this.data[`${type}Month`] || 
-            !this.data[`${type}Day`]) {
-          wx.showToast({ title: `请选择完整${type === 'deadline' ? '报名' : ''}时间`, icon: 'none' });
-          throw new Error('Validation failed');
-        }
+      console.log("验证时间字段:", {
+        start_time: event.start_time,
+        end_time: event.end_time,
+        registration_deadline: event.registration_deadline
       });
-    
-      // 构造请求数据
-      const postData = {
-        event_name: this.data.event_name,
-        poster: this.data.poster,
-        description: this.data.description,
-        location: this.data.location,
-        link: this.data.link,
-        start_time: this.formatTime('startTime').iso,
-        end_time: this.formatTime('endTime').iso,
-        registration_deadline: this.formatTime('deadline').iso
-      };
-    
-      // 发送请求
-      wx.request({
-        url: config.activity_submit,
-        method: 'POST',
-        header: {
-          'Authorization': `Bearer ${token}`,
-          'content-type': 'application/json'
-        },
-        data: postData,
-        success(res) {
-          if (res.statusCode === 200) {
-            wx.showToast({ title: '发布成功' });
-            setTimeout(() => wx.navigateBack(), 1500);
-          } else {
-            console.error('API错误:', res.data);
-            wx.showToast({ title: '服务器处理失败', icon: 'none' });
-          }
-        },
-        fail(error) {
-          console.error('网络错误:', error);
-          wx.showToast({ title: '网络连接失败', icon: 'none' });
-        }
+      
+      if (!event.start_time) {
+        console.log("开始时间验证失败");
+        wx.showToast({ title: '请选择举办时间', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.end_time) {
+        console.log("结束时间验证失败");
+        wx.showToast({ title: '请选择结束时间', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      if (!event.registration_deadline) {
+        console.log("报名截止时间验证失败");
+        wx.showToast({ title: '请选择报名截止时间', icon: 'none' });
+        this.data.isValid = false;
+      }
+      
+      console.log("表单验证通过！");
+      this.data.isValid = true;
+    } catch (error) {
+      console.error("表单验证过程中出错:", error);
+      this.data.isValid = false;
+    }
+  },
+
+  // 修改：提交表单，先上传海报，成功后再提交表单
+  onSubmit(e) {
+    console.log('点击了提交按钮', e);
+    console.log('表单数据:', this.data.event);
+    console.log('时间选择器数据:', this.data.timePicker);
+
+    const isValid = this.data.isValid;
+    if (!isValid) {
+      console.log("表单验证失败");
+      return;
+    }
+
+    wx.showLoading({ title: '提交中...' });
+
+    // 先上传海报
+    this.uploadPoster(this.data.event.poster)
+      .then(posterUrl => {
+        // 海报上传成功，更新posterUrl
+        this.setData({ 'event.posterUrl': posterUrl });
+        
+        // 准备提交表单数据
+        const postData = {
+          ...this.data.event,
+          poster: posterUrl  // 使用后端返回的URL
+        };
+
+        // 提交表单
+        return new Promise((resolve, reject) => {
+          wx.request({
+            url: `${API_BASE}/events/post`,
+            method: 'POST',
+            header: {
+              'Authorization': `Bearer ${token}`,
+              'content-type': 'application/json'
+            },
+            data: postData,
+            success: (res) => {
+              if (res.data.code === 200) {
+                resolve();
+              } else {
+                reject(res.data.message || '发布失败');
+              }
+            },
+            fail: (err) => {
+              reject('网络错误，请重试');
+            }
+          });
+        });
+      })
+      .then(() => {
+        wx.hideLoading();
+        wx.showToast({ title: '发布成功', icon: 'success' });
+        setTimeout(() => {
+          wx.navigateBack();
+        }, 1500);
+      })
+      .catch(err => {
+        console.error('发布失败:', err);
+        wx.hideLoading();
+        wx.showToast({ 
+          title: typeof err === 'string' ? err : '发布失败', 
+          icon: 'error' 
+        });
       });
-    }    
+  },
+
+  // 返回处理函数
+  handlerGobackClick() {
+    wx.showModal({
+      title: '确认返回',
+      content: '返回将丢失已填写的内容，是否确认？',
+      success: (res) => {
+        if (res.confirm) {
+          wx.navigateBack();
+        }
+      }
+    });
+  },
+
+  handlerGohomeClick() {
+    wx.showModal({
+      title: '确认返回首页',
+      content: '返回首页将丢失已填写的内容，是否确认？',
+      success: (res) => {
+        if (res.confirm) {
+          wx.reLaunch({ url: '/pages/index/index' });
+        }
+      }
+    });
+  }
 });
--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,6 @@
 const formatDateTime = isoString => {
   if (!isoString) return '';
-  
+
   const date = new Date(isoString);
   
   // 检查日期是否有效
@@ -18,24 +18,51 @@ const formatDateTime = isoString => {
   return `${year}/${month}/${day} ${hours}:${minutes}`;
 };
 
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
-
-
-// 将日期字符串从 "2024-02-13 14:30:00" 转换为 "2023.11.13"
-function formatDate(dateString) {
-  const date = new Date(dateString); // 解析日期字符串
-  const year = date.getFullYear(); // 获取年份
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 获取月份，+1 是因为月份从0开始
-  const day = date.getDate().toString().padStart(2, '0'); // 获取日期
-  return `${year}.${month}.${day}`; // 按照 "年.月.日" 的格式返回
-}
+/**
+ * 将时间组件转换为ISO 8601格式的时间字符串
+ * @param {Object} options - 时间组件对象
+ * @param {string|number} options.year - 年份
+ * @param {string|number} options.month - 月份（可包含"月"字）
+ * @param {string|number} options.day - 日期（可包含"日"字）
+ * @param {string|number} options.hour - 小时（可包含"时"字）
+ * @param {string|number} options.minute - 分钟（可包含"分"字）
+ * @returns {string} ISO格式的时间字符串，格式为YYYY-MM-DDThh:mm:00Z
+ */
+const formatToISOTime = (options) => {
+  // 检查必要参数
+  if (!options || !options.year || !options.month || !options.day || 
+      !options.hour || !options.minute) {
+    return '';
+  }
+  
+  // 提取数字并格式化为两位
+  const extractNumber = (value) => {
+    if (!value) return '00';
+    return value.toString().replace(/[^0-9]/g, '').padStart(2, '0');
+  };
+  
+  try {
+    const year = options.year.toString().replace(/[^0-9]/g, '');
+    const month = extractNumber(options.month);
+    const day = extractNumber(options.day);
+    const hour = extractNumber(options.hour);
+    const minute = extractNumber(options.minute);
+    
+    // 验证提取的值是否合法
+    if (!year || !month || !day || !hour || !minute) {
+      return '';
+    }
+    
+    // 构建并返回ISO格式时间
+    return `${year}-${month}-${day}T${hour}:${minute}:00Z`;
+  } catch (error) {
+    console.error('格式化时间错误:', error);
+    return '';
+  }
+};
 
 module.exports = {
-  formatDateTime: formatDateTime
+  formatDateTime: formatDateTime,
+  formatToISOTime: formatToISOTime
 }
 
